3de0550de5030acf07e97405dac56a3600ba965e
me@learnwithmia.com <me@learnwithmia.com>
2025-02-19 15:38:51 +1100

    split out subscription_id in policy-with-builtin-parameters

--- policy/builtin-with-parameters/main.tf
+++ policy/builtin-with-parameters/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_subscription_policy_assignment" "this" {
   for_each = { for each in var.policies : each.name => each }
   name                 = each.value.name
   policy_definition_id = each.value.policy_definition_id
-  subscription_id      = each.value.subscription_id
+  subscription_id      = var.subscription_id
   enforce              = each.value.enforce ? each.value.enforce : null
   parameters           = jsonencode({
     (each.value.parameter_name) = {
--- policy/builtin-with-parameters/variables.tf
+++ policy/builtin-with-parameters/variables.tf
@@ -2,10 +2,14 @@ variable "policies" {
   type = list(object({
     name                        = string
     policy_definition_id        = string
-    subscription_id             = string
+    # subscription_id             = string
     parameter_name              = string
     parameter_type              = string
     parameter_value             = string
     enforce                     = optional(bool, false)
   }))
+}
+
+variable "subscription_id" {
+  type = string
 }
